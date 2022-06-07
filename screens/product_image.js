01e29c1d00d4f47ecd2a646ee2ca2105526a169a
Ngaaje-Ashu <ngaajeashu12@gmail.com>
2022-06-07 17:40:33 +0100

    added 2 other screens

---
+++ screens/product_image.js
@@ -0,0 +1,49 @@
+import React ,{useState, useEffect}from 'react'
+import { StyleSheet, Text, View ,SafeAreaView ,TouchableOpacity ,Image ,Button ,Dimensions} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import CustomButton from '../Components/CustomButton';
+import CustomInput from '../Components/CustomInput';
+import CustomLink from '../Components/CustomLink';
+
+export default function product_image() {
+    const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+    const [ image, setImage ] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const galleryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+            setHasGalleryPermission(galleryStatus.status === 'granted');
+        })();
+    }, []);
+
+    const pickImage = async () => {
+        let result  =  await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4,3],
+            quality:1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled){
+            setImage(result.uri);
+        }
+    };
+
+    if (hasGalleryPermission === false){
+        return <Text>No access to internal storage</Text>
+    }
+
+  return (
+   <view style={{flex:1, justifyContent:'center'}}>
+     <Button title='pick Image' onPress={() => pickImage()} style={{marginTop:30}} /> 
+     {image && <Image source={{uri: image}} style={{flex:1/2}} />}
+
+   </view>
+  );
+}
+    
+
+  
+//export default Create
